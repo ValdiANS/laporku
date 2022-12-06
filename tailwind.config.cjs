565202fb85da700ed0441644b824782803c79cd9
Valdi <rvdans002@gmail.com>
2022-12-06 18:26:54 +0700

    extend app theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,16 +8,21 @@ module.exports = {
       },
 
       textColor: {
-        primary: '#636363',
+        primary: '#626262',
         secondary: '#F5F9FA',
       },
 
       borderColor: {
-        primary: '#D7D7D7',
+        primary: '#A2D3D7',
+        secondary: '#D7D7D7',
       },
 
       backgroundImage: {
         primaryGradient: 'linear-gradient(180deg, #A2D3D7 0%, #7FC0D2 100%);',
+      },
+
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
       },
     },
   },
