1339d9414c2c4029dc872dda617bf9b7c58d4702
Valdi <rvdans002@gmail.com>
2022-12-06 18:27:13 +0700

    add <App />

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { RouterProvider } from 'react-router-dom';
-import router from './routes/router';
-
 import { Provider } from 'react-redux';
 import { store } from './store';
 
+import '@animxyz/core';
+
 import './styles/index.css';
+
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
