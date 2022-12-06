7b52c2015361600575d0b99e8ce06c1a4f199b5d
Valdi <rvdans002@gmail.com>
2022-12-06 18:33:59 +0700

    add user and UI reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { userReducer } from './slice/user-slice';
 import { locationReducer } from './slice/location-slice';
+import { uiReducer } from './slice/ui-slice';
 
 const store = configureStore({
   reducer: {
+    user: userReducer,
     location: locationReducer,
+    ui: uiReducer,
   },
 });
 
