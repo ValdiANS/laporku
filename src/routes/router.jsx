87a6d101a516a86b429bbc8c1c94b8657c839507
Valdi <rvdans002@gmail.com>
2022-12-06 18:33:41 +0700

    update routes

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Register from '../pages/Register';
@@ -6,23 +6,45 @@ import UserLogin from '../pages/Login';
 import AdminDashboard from '../pages/Admin/Dashboard';
 import AdminLogin from '../pages/Admin/Login';
 import NotFound from '../pages/NotFound';
+import ViewReportSidebar from '../pages/Home/components/ViweReportSidebar';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
     errorElement: <NotFound />,
+    children: [
+      {
+        path: 'login',
+        element: <UserLogin />,
+      },
+
+      {
+        path: 'register',
+        element: <Register />,
+      },
+
+      {
+        path: 'view/:reportId',
+        element: <ViewReportSidebar />,
+      },
+    ],
   },
 
   {
-    path: '/register',
-    element: <Register />,
+    path: '/admin',
+    element: <Navigate to='/admin/dashboard' />,
   },
 
-  {
-    path: '/login',
-    element: <UserLogin />,
-  },
+  // {
+  //   path: '/register',
+  //   element: <Register />,
+  // },
+
+  // {
+  //   path: '/login',
+  //   element: <UserLogin />,
+  // },
 
   {
     path: '/admin/login',
