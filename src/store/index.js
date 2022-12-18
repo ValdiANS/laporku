d2cf0ded636d69f0c4f465cd7b48864c4e7d3a79
Valdi <rvdans002@gmail.com>
2022-12-18 16:17:10 +0700

    add reports reducer

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import { userReducer } from './slice/user-slice';
 import { locationReducer } from './slice/location-slice';
 import { uiReducer } from './slice/ui-slice';
+import { reportsReducer } from './slice/reports-slice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    reports: reportsReducer,
     location: locationReducer,
     ui: uiReducer,
   },
