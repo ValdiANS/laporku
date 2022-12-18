bc9926522893148f4575cd82c90b15a96c3a9325
Valdi <rvdans002@gmail.com>
2022-12-18 16:20:26 +0700

    add show modal functions with different type

--- src/store/slice/ui-slice.js
+++ src/store/slice/ui-slice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const uiInitialState = {
-  isLoading: false,
+  isLoading: true,
   showInfoModal: false,
   infoModalType: '',
 };
@@ -14,22 +14,99 @@ const uiSlice = createSlice({
       state.isLoading = action.payload;
     },
 
-    showInfoModal(state, action) {
-      /*
-        action: {
-          payload: {
-            type: ''
-          }
-        }
-      */
+    hideInfoModal(state, action) {
+      state.showInfoModal = false;
+      state.infoModalType = '';
+    },
 
-      state.infoModalType = action.payload.type;
+    showAddReportModal(state, action) {
+      state.infoModalType = 'addReport';
       state.showInfoModal = true;
     },
 
-    showInfoModal(state, action) {
-      state.showInfoModal = false;
-      state.infoModalType = '';
+    showAddReportSuccessModal(state, action) {
+      state.infoModalType = 'addReportSuccess';
+      state.showInfoModal = true;
+    },
+
+    showAddReportErrorModal(state, action) {
+      state.infoModalType = 'addReportError';
+      state.showInfoModal = true;
+    },
+
+    showRegisterSuccessModal(state, action) {
+      state.infoModalType = 'registerSuccess';
+      state.showInfoModal = true;
+    },
+
+    showRegisterErrorModal(state, action) {
+      state.infoModalType = 'registerError';
+      state.showInfoModal = true;
+    },
+
+    showLoginSuccessModal(state, action) {
+      state.infoModalType = 'loginSuccess';
+      state.showInfoModal = true;
+    },
+
+    showLoginErrorModal(state, action) {
+      state.infoModalType = 'loginError';
+      state.showInfoModal = true;
+    },
+
+    showGetAllReportsErrorModal(state, action) {
+      state.infoModalType = 'getAllReportsError';
+      state.showInfoModal = true;
+    },
+
+    showGetReportErrorModal(state, action) {
+      state.infoModalType = 'getReportError';
+      state.showInfoModal = true;
+    },
+
+    showUserNotLoginErrorModal(state, action) {
+      state.infoModalType = 'userNotLoginError';
+      state.showInfoModal = true;
+    },
+
+    showUserNotLoginReportInvalidReportErrorModal(state, action) {
+      state.infoModalType = 'userNotLoginReportInvalidReportError';
+      state.showInfoModal = true;
+    },
+
+    showVoteErrorModal(state, action) {
+      state.infoModalType = 'voteError';
+      state.showInfoModal = true;
+    },
+
+    showInvalidReportErrorModal(state, action) {
+      state.infoModalType = 'invalidReportError';
+      state.showInfoModal = true;
+    },
+
+    showInvalidReportAcceptedModal(state, action) {
+      state.infoModalType = 'invalidReportAccepted';
+      state.showInfoModal = true;
+    },
+
+    showGetInvalidReportsErrorModal(state, action) {
+      state.infoModalType = 'getInvalidReportsError';
+      state.showInfoModal = true;
+    },
+
+    showDeleteReportSuccessModal(state, action) {
+      state.infoModalType = 'deleteReportSuccess';
+      state.showInfoModal = true;
+    },
+
+    showDeleteReportErrorModal(state, action) {
+      state.infoModalType = 'deleteReportError';
+      state.showInfoModal = true;
+    },
+
+    showGetUserLocationErrorModal(state, action) {
+      state.infoModalType = 'getUserLocationError';
+      state.showInfoModal = true;
     },
   },
 });
