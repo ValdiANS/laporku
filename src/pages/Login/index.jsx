032f9a3682dbbf11aa682c81f954ec2eb2afdb3b
Valdi <rvdans002@gmail.com>
2022-12-18 16:14:52 +0700

    add login page functionality

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,22 +1,134 @@
-import { Fragment } from 'react';
+import { useState, Fragment, useRef } from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { XyzTransition } from '@animxyz/react';
 
-import { fakeLogin } from '../../store/slice/user-slice';
+import { API } from '../../lib/api';
+
+import { uiActions } from '../../store/slice/ui-slice';
+import { userActions } from '../../store/slice/user-slice';
 
 import Logo from '../../components/SVG/Logo';
+
+const errorType = {
+  emailEmpty: 'Email tidak boleh kosong',
+  passwordEmpty: 'Password tidak boleh kosong',
+  wrongEmailFormat: 'Gunakan format email yang benar',
+  wrongPassword: 'Password salah',
+};
+
+const checkInputValidity = (email, password) => {
+  const inputValidity = {
+    valid: true,
+    email: {
+      valid: true,
+      message: '',
+    },
+    password: {
+      valid: true,
+      message: '',
+    },
+  };
+
+  if (email.length === 0) {
+    inputValidity.valid = false;
+    inputValidity.email.valid = false;
+    inputValidity.email.message = errorType.emailEmpty;
+  } else if (!email.includes('@')) {
+    inputValidity.valid = false;
+    inputValidity.email.valid = false;
+    inputValidity.email.message = errorType.wrongEmailFormat;
+  }
+
+  if (password.length === 0) {
+    inputValidity.valid = false;
+    inputValidity.password.valid = false;
+    inputValidity.password.message = errorType.passwordEmpty;
+  }
+
+  return inputValidity;
+};
 
 const Login = () => {
   const dispatch = useDispatch();
 
   const isUserLogin = useSelector((state) => state.user.isLogin);
 
-  const loginSubmitHandler = (e = new SubmitEvent()) => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const [isEmailValid, setIsEmailValid] = useState({
+    valid: true,
+    message: '',
+  });
+  const [isPasswordValid, setIsPasswordValid] = useState({
+    valid: true,
+    message: '',
+  });
+  const [isWrongPassword, setIsWrongPassword] = useState(false);
+
+  const loginSubmitHandler = async (e = new SubmitEvent()) => {
     e.preventDefault();
 
-    dispatch(fakeLogin());
+    setIsEmailValid({
+      valid: true,
+      message: '',
+    });
+    setIsPasswordValid({
+      valid: true,
+      message: '',
+    });
+    setIsWrongPassword(false);
+    dispatch(uiActions.setIsLoading(true));
+
+    const formValidity = checkInputValidity(
+      emailRef.current.value,
+      passwordRef.current.value
+    );
+
+    if (!formValidity.valid) {
+      setIsEmailValid(formValidity.email);
+      setIsPasswordValid(formValidity.password);
+
+      dispatch(uiActions.setIsLoading(false));
+      return;
+    }
+
+    try {
+      // Login API
+      const loginResponse = await API.Login({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      });
+
+      if (loginResponse.errorType === 'wrong-password') {
+        setIsWrongPassword(true);
+        dispatch(uiActions.setIsLoading(false));
+
+        return;
+      }
+
+      if (loginResponse.errorType === 'invalid-account') {
+        dispatch(uiActions.showLoginErrorModal());
+        dispatch(uiActions.setIsLoading(false));
+
+        return;
+      }
+
+      dispatch(userActions.login(loginResponse.data));
+
+      dispatch(uiActions.showLoginSuccessModal());
+    } catch (error) {
+      console.log('Gagal Login!');
+      console.log(error.message);
+      console.log('Error');
+      console.log(error);
+
+      dispatch(uiActions.showLoginErrorModal());
+    }
+
+    dispatch(uiActions.setIsLoading(false));
   };
 
   return (
@@ -48,13 +160,24 @@ const Login = () => {
                   </label>
 
                   <input
-                    type='email'
+                    ref={emailRef}
+                    type='text'
                     name='email'
                     id='email'
                     placeholder='Isi email kamu di sini...'
-                    className='w-full p-5 rounded-lg'
+                    className={`w-full p-5 rounded-lg border-2 border-solid ${
+                      isEmailValid.valid
+                        ? ''
+                        : 'border-[#FF0000] placeholder:text-[#FF0000]'
+                    }`}
                     autoFocus
                   />
+
+                  {!isEmailValid.valid && (
+                    <small className='block text-base text-[#FF0000]'>
+                      {isEmailValid.message}
+                    </small>
+                  )}
                 </div>
 
                 <div className='input space-y-2'>
@@ -66,12 +189,29 @@ const Login = () => {
                   </label>
 
                   <input
+                    ref={passwordRef}
                     type='password'
                     name='password'
                     id='password'
                     placeholder='Isi password kamu di sini...'
-                    className='w-full p-5 rounded-lg'
+                    className={`w-full p-5 rounded-lg border-2 border-solid ${
+                      isPasswordValid.valid
+                        ? ''
+                        : 'border-[#FF0000] placeholder:text-[#FF0000]'
+                    }`}
                   />
+
+                  {!isPasswordValid.valid && !isWrongPassword && (
+                    <small className='block text-base text-[#FF0000]'>
+                      {isPasswordValid.message}
+                    </small>
+                  )}
+
+                  {isWrongPassword && (
+                    <small className='block text-base text-[#FF0000]'>
+                      {errorType.wrongPassword}
+                    </small>
+                  )}
                 </div>
               </div>
 
@@ -85,7 +225,7 @@ const Login = () => {
 
                 <button
                   type='submit'
-                  className='px-8 py-4 bg-white font-bold text-xl text-primary text-center rounded-lg'
+                  className='px-8 py-4 bg-white font-bold text-xl text-primary text-center rounded-lg transition-all hover:-translate-y-1 active:translate-y-0 hover:brightness-110 active:brightness-100'
                 >
                   Masuk
                 </button>
