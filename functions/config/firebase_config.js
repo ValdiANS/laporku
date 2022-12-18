a3b1fca2ef0129ea5b1b833d65dfe0b2781e9289
Valdi <rvdans002@gmail.com>
2022-12-18 17:02:25 +0700

    add dotenv.config()

--- functions/config/firebase_config.js
+++ functions/config/firebase_config.js
@@ -1,6 +1,10 @@
+import { config } from 'dotenv';
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+
+config();
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
