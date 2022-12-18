935f2e15806632b30f704a97fd4abd8cbf993394
Valdi <rvdans002@gmail.com>
2022-12-18 15:32:17 +0700

    set z-index to 99999

--- src/components/SidebarLoadingScreen.jsx
+++ src/components/SidebarLoadingScreen.jsx
@@ -9,7 +9,7 @@ const SidebarLoadingScreen = ({ show = true }) => (
     xyz='fade in-down-100% out-up-100%'
   >
     {show && (
-      <div className='w-full h-screen absolute top-0 left-0 bg-primaryGradient grid place-items-center p-8'>
+      <div className='w-full h-screen absolute top-0 left-0 bg-primaryGradient grid place-items-center p-8 z-[99999]'>
         <Logo className='w-full max-w-[550px] h-fit animate-pulse' />
       </div>
     )}
